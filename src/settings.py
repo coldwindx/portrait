edd59cdd16b3391a7e0dd0c64f12dbb2b4d85239
coldwindx <15653913658@163.com>
2022-10-17 21:39:17 +0800

    .

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 import os
 
-PROJECT_PATH = os.getcwd()
+PROJECT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
 
 HLM_TXT_PATH = PROJECT_PATH + '/data/红楼梦.txt'
 STOP_WORDS_TXT_PATH = PROJECT_PATH + '/data/stopwords.txt'
